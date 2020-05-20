157f1bea206401c34bf6de7c292b4f197bc38760
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-20 16:43:13 -0300

    Projeto criado

---
+++ src/test/java/com/estudando/coursemongodb/CoursemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudando.coursemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
