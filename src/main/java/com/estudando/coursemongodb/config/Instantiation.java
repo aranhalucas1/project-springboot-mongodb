d32e761e00ba0425ad0ac97b00953fe34efc336e
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-23 15:02:57 -0300

    Projecao dos dados do autor com DTO FIXED

--- src/main/java/com/estudando/coursemongodb/config/Instantiation.java
+++ src/main/java/com/estudando/coursemongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.estudando.coursemongodb.domain.Post;
 import com.estudando.coursemongodb.domain.User;
+import com.estudando.coursemongodb.dto.AuthorDTO;
 import com.estudando.coursemongodb.repository.PostRepository;
 import com.estudando.coursemongodb.repository.UserRepository;
 
@@ -33,11 +34,11 @@ public class Instantiation implements CommandLineRunner {
 		User u1 = new User(null, "Lucas Aranha", "lucinhas@gmail.com");
 		User u2 = new User(null, "Renato Aranha", "renato@gmail.com");
 		User u3 = new User(null, "Liciane Aranha", "liciane@gmail.com");
-		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
-		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2)); 
 	}
 
